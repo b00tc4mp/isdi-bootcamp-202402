948ba84eb6d0a1971d8a26c0bd42b9e7a3bae5be
Irene <irene.roldan.montiel@gmail.com>
2024-02-15 14:25:29 +0100

    add challenge #143

---
+++ staff/irene-roldan/complex-structure/challenge.js
@@ -0,0 +1,17 @@
+var a = {
+    animal: "dog",
+    size: "medium",
+    gender: "female",
+    esterilized: true,
+    birthday: 2017,
+    calculateAge: function (){
+        this.age = 2024 - this.birthday
+    },
+    family: ["Maria", "Andreu", "Aina"],
+}
+
+1.  Cambiar a macho el género.
+2. Añadir a familia otra persona.
+3. Restar de la familia la 2ª persona.
+4. Calcular la edad del perro y sumarle 5 años más. 
+5. Si el perro es mayor de 15 años mostrar un mensaje de "Soy mayor de 15 años". 
