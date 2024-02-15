eda72cadbe8d1ffd3ca66a0e26afa08220716500
Judit-Camps <campsjudit99@gmail.com>
2024-02-15 17:37:55 +0100

    add find function code #201

---
+++ staff/judit-camps/arrays/find.js
@@ -0,0 +1,29 @@
+delete Array.prototype.find
+
+function find(arr, fun) {
+    for (var i = 0; i < arr.length; i++) {
+        if (fun(arr[i])) {
+            return arr[i]
+        }
+    }
+}
+
+console.log('CASE 1')
+var a = [2, 5, 30, 50]
+var found = find(a, (elem) => elem > 10)
+console.log(found)
+
+
+console.log('CASE 2')
+const inventory = [
+    { name: "apples", quantity: 2 },
+    { name: "bananas", quantity: 0 },
+    { name: "cherries", quantity: 5 },
+];
+
+function isCherries(fruit) {
+    return fruit.name === "cherries";
+}
+
+console.log(find(inventory, isCherries));
+// { name: 'cherries', quantity: 5 }
