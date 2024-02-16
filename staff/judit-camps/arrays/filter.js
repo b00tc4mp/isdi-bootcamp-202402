f7dd7e56cedc0016a069deec455741651f2598a1
Judit-Camps <campsjudit99@gmail.com>
2024-02-16 09:15:18 +0100

    add filter function code #201

---
+++ staff/judit-camps/arrays/filter.js
@@ -0,0 +1,29 @@
+delete Array.prototype.findIndex
+
+function filter(arr, fun) {
+    var newArr = []
+    for (var i = 0; i < arr.length; i++) {
+        if (fun(arr[i])) {
+            newArr[newArr.length] = arr[i]
+        }
+    }
+    return newArr
+}
+
+console.log('CASE 1')
+var words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
+var result = filter(words, (word) => word.length > 6);
+console.log(result);
+// Expected output: Array ["exuberant", "destruction", "present"]
+
+
+
+console.log('CASE 2')
+var l = [12, 5, 8, 130, 44]
+function isBigEnough(value) {
+    return value >= 10;
+}
+
+var filtered = filter(l, isBigEnough);
+console.log(filtered)
+// filtered is [12, 130, 44]
