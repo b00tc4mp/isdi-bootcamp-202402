e9370c4d242d3c021ba8187a4fc5f4dbaf7f26bb
Pere <tisores92@hotmail.com>
2024-02-17 14:02:17 +0100

    implement all working cases of slice #198

--- staff/pere-hernandez/arrays/slice.js
+++ staff/pere-hernandez/arrays/slice.js
@@ -24,22 +24,49 @@ function sliceArray(array, start, end){
             slicedArray[count] = array[i]
             count++
         }
-        array.length = 0;
+        array.length = 0
         for (let i = 0; i < slicedArray.length; i++) {
-          array[i] = slicedArray[i];
+          array[i] = slicedArray[i]
         }
     } else if (start < 0 && end === undefined){
         for (let i = array.length + start; i < array.length; i++){
             newArray[count] = array[i]
             count++
         }
+        let slicedArray = []
+        count = 0
+        for (let i = 0; i < start; i++){
+            slicedArray[count] = array[i]
+            count++
+        }
+        array.length = array.length + start        
     } else if (start > -1 && end < 0){
         for (let i = start; i < (end * -1) +1; i++){
             newArray[count] = array[i]
             count++
         }
+        count = 0
+        let slicedArray = []
+        for (let i = 0; i < start; i++){
+            slicedArray[count] = array[i]
+            count++
+        }
+        for (let i = (end * -1) +1; i < array.length; i++){
+            slicedArray[count] = array[i]
+            count++
+        }
+        array.length = 0
+        for (let i = 0; i < slicedArray.length; i++) {
+          array[i] = slicedArray[i]
+        }
     } else if (arguments.length < 2){
-        return array
+        newArray = []
+        for (let i = 0; i < array.length; i++){
+            newArray[i] = array[i]
+        }
+        array.length = 0
+        delete array[0]
+        return newArray
     }
     return newArray
 }
@@ -66,15 +93,21 @@ let arr2 = ['azúcar', 'especias', 'muchas cosas bonitas', 'sustancia X']
 result = sliceArray(arr2, -3)
 console.log(result)
 //['especias', 'muchas cosas bonitas', 'sustancia X']
+console.log(arr2)
+//['azúcar']
 
 //CASE 4
 let arr3 = ['azúcar', 'especias', 'muchas cosas bonitas', 'sustancia X']
 result = sliceArray(arr3, 1, -2)
 console.log(result)
 //['especias', 'muchas cosas bonitas']
+console.log(arr3)
+//['azúcar', 'sustancia X']
 
 //CASE 5
 let arr4 = ['azúcar', 'especias', 'muchas cosas bonitas', 'sustancia X']
 result = sliceArray(arr4)
 console.log(result)
 //['azúcar', 'especias', 'muchas cosas bonitas', 'sustancia X']
+console.log(arr4)
+//[]
