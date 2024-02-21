7eff10598a2290202b42f1c4e2a9d9fcd5e89447
Judit-Camps <campsjudit99@gmail.com>
2024-02-21 18:06:06 +0100

    add object functions #345

---
+++ staff/judit-camps/objects/extract.js
@@ -0,0 +1,50 @@
+/**
+ * 
+ * @param {*} object 
+ * @param {*} callback 
+ */
+function extract(object, callback) {
+    var extracted
+    var indexExtracted
+    for (var i = 0; i < object.length; i++) {
+        if (callback(object[i])) {
+            extracted = object[i]
+            indexExtracted = i
+        }
+    }
+    for (var i = indexExtracted; i < object.length; i++) {
+        object[i] = object[i + 1]
+    }
+    delete object[object.length]
+    object.length--
+    return extracted
+}
+
+console.log('CASE 1: extract user pepito form users')
+
+var users = {
+    0: { name: 'Wendy', age: 19 },
+    1: { name: 'Peter', age: 20 },
+    2: { name: 'Pepito', age: 50 },
+    3: { name: 'Campa', age: 30 },
+    4: { name: 'James', age: 40 },
+    length: 5
+}
+
+var user = extract(users, function (user) {
+    return user.name === 'Pepito'
+})
+
+console.log(user)
+// { name: 'Pepito', age: 50 }
+
+console.log(users)
+/*
+{
+    0: { name: 'Wendy', age: 19 },
+    1: { name: 'Peter', age: 20 },
+    2: { name: 'Campa', age: 30 },
+    3: { name: 'James', age: 40 },
+    length: 4
+}
+*/
