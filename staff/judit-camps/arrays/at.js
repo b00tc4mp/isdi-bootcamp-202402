94e1fa54e62375a8da51712abf9500143158e59b
Judit-Camps <campsjudit99@gmail.com>
2024-02-23 11:49:59 +0100

    automatize tests with assertions in at and concat functions #201

--- staff/judit-camps/arrays/at.js
+++ staff/judit-camps/arrays/at.js
@@ -8,38 +8,25 @@ function at(array, index) {
 
 }
 
-// CASE 1
-
+console.log('CASE 1: show value at position 3 -> 400')
 var nums = [100, 200, 300, 400, 500]
-
 var num = at(nums, 3)
+console.assert(num === 400, 'num 400')
 
-console.log(num)
-// 400
 
-// CASE 2
-
+console.log('CASE 2: show value at position 4 -> " "')
 var chars = ['h', 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o']
-
 var char = at(chars, 4)
+console.assert(char === ' ', 'char " "')
 
-console.log(char)
-// ' '
 
-// CASE 3
-
+console.log('CASE 3: show value at position -3 -> n')
 var chars = ['h', 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o']
-
 var char = at(chars, -3)
+console.assert(char === 'n', 'char n')
 
-console.log(char)
-// 'n'
 
-// CASE 4
-
+console.log('CASE 4: show value at position -30 -> undefined')
 var chars = ['h', 'o', 'l', 'a', ' ', 'm', 'u', 'n', 'd', 'o']
-
 var char = at(chars, -30)
-
-console.log(char)
-// undefined
+console.assert(char === undefined)
--- staff/judit-camps/arrays/concat.js
+++ staff/judit-camps/arrays/concat.js
@@ -14,45 +14,59 @@ function concat(...arrays) {
     return newArray
 }
 
-// CASE 1
+console.log('CASE 1: Concat 3 arrays into one -> ["a", "b", "c", "d", "e", "f"]')
 var array1 = ['a', 'b', 'c'];
 var array2 = ['d', 'e', 'f'];
 var array3 = ['g', 'h', 'i'];
 var array4 = concat(array1, array2, array3);
+console.assert(array4[0] === 'a', 'a');
+console.assert(array4[1] === 'b', 'b');
+console.assert(array4[2] === 'c', 'c');
+console.assert(array4[3] === 'd', 'd');
+console.assert(array4[4] === 'e', 'e');
+console.assert(array4[5] === 'f', 'f');
+console.assert(array4[6] === 'g', 'g');
+console.assert(array4[7] === 'h', 'h');
+console.assert(array4[8] === 'i', 'i');
 
-console.log(array4);
-// ["a", "b", "c", "d", "e", "f"]
 
-
-// CASE 2
+console.log('CASE 2: Concat 2 arrays -> ["a", "b", "c", 1, 2, 3]')
 var letters = ["a", "b", "c"];
 var numbers = [1, 2, 3];
-
 var n = concat(letters, numbers);
-// ['a', 'b', 'c', 1, 2, 3]
+console.assert(n[0] === 'a', 'a')
+console.assert(n[1] === 'b', 'b')
+console.assert(n[2] === 'c', 'c')
+console.assert(n[3] === 1, 1)
+console.assert(n[4] === 2, 2)
+console.assert(n[5] === 3, 3)
 
 
-// CASE 3
+console.log('CASE 3: Concat 1 array with numbers and arrays -> ["a", "b", "c", 1, 2, 3]')
 var alpha = ["a", "b", "c"];
-var alphaNumeric = concat(alpha, 1, [2, 3]);
-
-console.log(alphaNumeric);
-// ['a', 'b', 'c', 1, 2, 3]
-
-
-// CASE 4
-var num1 = [[1]];
-var num2 = [2, [3]];
-var nums = concat(num1, num2);
-
-console.log(nums);
-// [[1], 2, [3]]
+var alphaNumeric = concat(alpha, 1, [2, 3])
+console.assert(alphaNumeric[0] === 'a', 'a')
+console.assert(alphaNumeric[1] === 'b', 'b')
+console.assert(alphaNumeric[2] === 'c', 'c')
+console.assert(alphaNumeric[3] === 1, 1)
+console.assert(alphaNumeric[4] === 2, 2)
+console.assert(alphaNumeric[5] === 3, 3)
 
 
-// CASE 5
+console.log('CASE 4: Concat 2 arrays with arrays inside -> [[1], 2, [3]]')
+var num1 = [[1]]
+var num2 = [2, [3]]
+var nums = concat(num1, num2)
+var correct = [[1], 2, [3]]
+console.assert(nums[0][0] === correct[0][0], '[1]')
+console.assert(nums[1] === correct[1], '2')
+console.assert(nums[2][0] === correct[2][0], '[3]')
+
+
+console.log("CASE 5: Concat an array with a string -> [1, 2, 'hello']")
 var num = [1, 2];
 var w = 'hello'
 var newA = concat(num, w)
-
-console.log(newA)
-// [1, 2, 'hello']
+console.assert(newA[0] === 1, '1')
+console.assert(newA[1] === 2, '2')
+console.assert(newA[2] === 'hello', 'hello')
