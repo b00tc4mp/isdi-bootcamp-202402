9c9c98977890170aca8816e30d171fb681526378
Irene <irene.roldan.montiel@gmail.com>
2024-02-25 19:53:15 +0100

    concat #183

--- staff/irene-roldan/strings/concat.js
+++ staff/irene-roldan/strings/concat.js
@@ -1,11 +1,19 @@
 delete String.prototype.concat
 
-function concat(element1, element2, separator){
-    //crear una variable auxiliar 'result' que sume valores de element + separator
+function concat(element1, element2, separator){ //SI SOLO CONCADENA DOS ELEMENTOS
     var result = element1 + separator + element2
-    //función que devuelva variable auxiliar 'result'
+
     return result
-    
+}
+
+
+function concat(elements, separator){
+    var result = elements[0] //se iniciliza con el primer elemento 
+
+    for (let i = 1; i < elements.length; i++) { //empieza a contar desde el segundo elemento elements[1] = 'How'
+        result += separator + elements[i] // world = world + ' ' + How 
+    }
+    return result
 }
 
 console.log('CASE 1')
@@ -13,6 +21,16 @@ console.log('CASE 1')
 var str1 = 'Hello'
 var str2 = 'World'
 
-var char = concat(str1, str2, " ")
+var char = concat([str1, str2], ' ')
 
-console.assert(char === 'Hello World')
+console.assert(char === 'Hello World', ' Expected Hello World')
+
+console.log('CASE 2')
+
+var str1 = 'World'
+var str2 = 'How'
+var str3 = 'are'
+var str4 = 'you'
+
+var char = concat([str1, str2, str3, str4], ' ')
+console.assert(char === 'World How are you', 'Expected "World How are you"')
