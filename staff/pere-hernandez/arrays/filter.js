7312e2c8e84f72e3ed2a16e498ffc3eb39bee732
Pere <tisores92@hotmail.com>
2024-02-26 11:50:31 +0100

    Implement errors and asserts on filter.js, fimd.js and findIndex.js #198

--- staff/pere-hernandez/arrays/filter.js
+++ staff/pere-hernandez/arrays/filter.js
@@ -1,14 +1,25 @@
 delete Array.prototype.filter
 
 function arrayFilter(array, index, callback) {
+    if (array instanceof Array === false)
+        throw new TypeError (array + ' is not an Array')
+
     let newArray = []
     if (arguments.length < 3){
         callback = index
+        if (callback instanceof Function === false)
+            throw new TypeError (callback + ' is not a Function')
+
         for (let i = 0; i < array.length; i++){
             if (callback(array[i]) === true)
                 newArray[newArray.length] = array[i]
         }
     } else {
+        if (callback instanceof Function === false)
+            throw new TypeError (callback + ' is not a Function')
+        if (!Number.isInteger(index))
+            throw new TypeError (index + ' is not an Integer')
+
         for (let i = index; i < array.length; i++){
             if (callback(array[i]) === true)
                 newArray[newArray.length] = array[i]
@@ -17,14 +28,58 @@ function arrayFilter(array, index, callback) {
     return newArray
 }
 
+function assert(array1, array2){
+    for (var i = 0; i < array1.length; i++){
+        console.assert(array1[i] === array2[i], array1[i])
+    }
+}
+
 console.log('CASE 1')
 
 let arr = ['yellow', 'red', 'blue', 'green']
 let result = arrayFilter(arr, (x) => x.length > 3)
-console.assert(result === "['yellow, 'blue', 'green']")
-//['yellow', 'blue', 'green']
+let expectedResult = ['yellow', 'blue', 'green']
+assert(result, expectedResult)
+
 
 console.log('CASE 2')
 result = arrayFilter(arr, 1, (x) => x.length > 3)
-console.log(result)
-//['blue', 'green']
+expectedResult = ['blue', 'green']
+assert(result, expectedResult)
+
+
+console.log('CASE 3')
+let str = "I'm not an Array"
+try {
+    result = arrayFilter(str, (x) => x.length > 3)
+} catch (error){
+    console.assert(error.name === 'TypeError', 'name')
+    console.assert(error.message === "I'm not an Array is not an Array", 'message')
+}
+
+
+console.log('CASE 4')
+try {
+    result = arrayFilter(arr, 3)
+} catch (error){
+    console.assert(error.name === 'TypeError', 'name')
+    console.assert(error.message === "3 is not a Function", 'message')
+}
+
+
+console.log('CASE 5')
+try {
+    result = arrayFilter(arr, 3, 3)
+} catch (error){
+    console.assert(error.name === 'TypeError', 'name')
+    console.assert(error.message === "3 is not a Function", 'message')
+}
+
+
+console.log('CASE 6')
+try {
+    result = arrayFilter(arr, str, 3)
+} catch (error){
+    console.assert(error.name === 'TypeError', 'name')
+    console.assert(error.message === "3 is not a Function", 'message')
+}
--- staff/pere-hernandez/arrays/find.js
+++ staff/pere-hernandez/arrays/find.js
@@ -1,14 +1,42 @@
 delete Array.prototype.find
 
 function findArray(array, formula) {
+    if (array instanceof Array === false)
+        throw new TypeError(array + ' is not an Array')
+    if (formula instanceof Function === false)
+        throw new TypeError(formula + ' is not a Function')
+    
     for (let i = 0; i < array.length; i++){
         if (formula(array[i]) === true)
             return array[i]
     }
 }
 
-//CASE 1
+console.log('CASE 1')
 let arr = [1, 2, 3, 4, 5]
 let result = findArray(arr, (x) => x > 2)
-console.log(result)
-//2
+console.assert(result === 3, '3')
+
+
+console.log('CASE 2')
+result = findArray(arr, (x) => x > 10)
+console.assert(result === undefined, 'undefined')
+
+
+console.log('CASE 3')
+let str = 'I am a String'
+try {
+    result = findArray(str, (x) => x > 10)
+} catch (error){
+    console.assert(error.name === 'TypeError', 'name')
+    console.assert(error.message === 'I am a String is not an Array')
+}
+
+
+console.log('CASE 4')
+try {
+    result = findArray(arr, str)
+} catch (error){
+    console.assert(error.name === 'TypeError', 'name')
+    console.assert(error.message === 'I am a String is not a Function')
+}
