bc154d98d0852d0f47a318a41f59fb74af0b58f7
Irene <irene.roldan.montiel@gmail.com>
2024-02-26 10:18:55 +0100

    edit #196

---
+++ staff/irene-roldan/arrays/testing-tools.js
@@ -0,0 +1,6 @@
+//const testingTools = require('./testing-tools') --> se añade en el fichero que se necesite 
+//testingTools.assertHasValues(nums, 1,2,3)
+
+module.exports = {
+    assertHasValues: assertHasValues,
+}
