35dbcdc68c5abc3f5b4b1535a3a4366c8be29494
Pere <tisores92@hotmail.com>
2024-02-28 09:43:18 +0100

    Implement person #391

--- staff/pere-hernandez/prototype/person/Person.js
+++ staff/pere-hernandez/prototype/person/Person.js
@@ -8,6 +8,40 @@ function Person (name, surname, height, weight, age, sex, skinTone, eyeColor, ha
     this.skinTone = skinTone
     this.eyeColor = eyeColor
     this.hairColor = hairColor
+    this.speed = 0
+    this.silent = true
+    this.message = ''
+    this.alive = true
 }
+
+
+Person.prototype.walk = function(){
+    this.speed = 4
+}
+
+Person.prototype.run = function(){
+    this.speed = 9
+}
+
+Person.prototype.stopMoving = function(){
+    this.speed = 0
+}
+
+Person.prototype.speak = function(string){
+    if (string instanceof String !== false)
+        throw new TypeError(string + ' is not a String')
+    this.silent = false
+    this.message = string
+}
+
+Person.prototype.stopTalking = function(){
+    this.silent = true
+    this.message = ''
+}
+
+Person.prototype.die = function(){
+    this.alive === false
+}
+
 
 module.exports = Person
