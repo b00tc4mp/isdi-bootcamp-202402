c911ce091953053e218721f596976e2b72a34356
Judit-Camps <campsjudit99@gmail.com>
2024-02-28 09:50:17 +0100

    add first properties for person #399

--- staff/judit-camps/prototypes/person/Person.js
+++ staff/judit-camps/prototypes/person/Person.js
@@ -1,15 +1,26 @@
 function Person(name, age, hairColor,) {
     this.name = name
     this.age = age
+    this.status = 'awake'
 
 }
 
-Person.prototype.walk = function () {
-
+Person.prototype.walk = function (where) {
+    console.log("I'm walking to the " + where)
 }
 
-Person.prototype.speak = function () {
+Person.prototype.speak = function (str) {
+    console.log(str)
+}
 
+Person.prototype.sleep = function () {
+    this.status = 'asleep'
+    console.log('ZzzZzz')
+}
+
+Person.prototype.wakeUp = function () {
+    this.status = 'awake'
+    console.log('Good Morning!')
 }
 
 
--- staff/judit-camps/prototypes/person/Person.test.js
+++ staff/judit-camps/prototypes/person/Person.test.js
@@ -3,6 +3,14 @@ var Person = require('./Person')
 console.log('TEST Person')
 
 console.log('CASE constructor')
-var p = new Person('Judit', 24)
-console.assert(p.name === 'Judit', 'name is Judit')
-console.assert(p.age === 24, 'age is 24')
+var p = new Person('Pepito', 39)
+console.assert(p.name === 'Pepito', 'name is Pepito')
+console.assert(p.age === 39, 'age is 39')
+
+p.wakeUp()
+console.assert(p.status === 'awake', 'status should be awake')
+
+p.walk('store')
+
+p.sleep()
+console.assert(p.status === 'asleep', 'status should be asleep')
