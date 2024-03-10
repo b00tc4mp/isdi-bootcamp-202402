c1937e42c8337ea548c0e1898d79ce2062341671
JordiRZ <jordirz199444@gmail.com>
2024-03-10 19:30:31 +0100

    add updated isdigram code #327

--- staff/jordi-relano/isdigram/app/home/index.js
+++ staff/jordi-relano/isdigram/app/home/index.js
@@ -5,7 +5,6 @@
         return
     }
 
-
     var title = document.querySelector('h1')
     var logoutButton = document.querySelector('#logout-button')
     var createPostSection = document.querySelector('#create-post-section')
@@ -13,7 +12,10 @@
     var createPostCancelButton = createPostSection.querySelector('#create-post-cancel-button')
     var createPostButton = document.querySelector('#create-post-button')
     var postListSection = document.querySelector('#post-list-section')
-
+    var chatButton = document.querySelector('#chat-button')
+    var chatSection = document.querySelector('#chat-section')
+    var footer = document.querySelector('#footer')
+    var homeButton = document.querySelector('#home-button')
 
     try {
         var user = logic.retrieveUser()
@@ -21,13 +23,12 @@
         title.innerText = 'Hello, ' + user.name + '!'
     } catch (error) {
         console.error(error)
+
         alert(error.message)
     }
 
     logoutButton.onclick = function () {
         logic.logoutUser()
-
-
 
         location.href = '../login'
     }
@@ -106,15 +107,59 @@
                     }
 
                     article.appendChild(deleteButton)
-
                 }
 
                 postListSection.appendChild(article)
             })
         } catch (error) {
+            console.error(error)
+
             alert(error.message)
         }
     }
 
     renderPosts()
+
+    chatButton.onclick = function () {
+        postListSection.style.display = 'none'
+        footer.style.display = 'none'
+        chatButton.style.display = 'none'
+
+        homeButton.style.display = 'block'
+        chatSection.style.display = 'block'
+
+        var userList = chatSection.querySelector('#user-list')
+
+        userList.innerHTML = ''
+
+        try {
+            var users = logic.retrieveUsers()
+
+            users.forEach(function (user) {
+                var item = document.createElement('li')
+
+                if (user.status === 'online')
+                    item.classList.add('user-list__item--online')
+                else if (user.status === 'offline')
+                    item.classList.add('user-list__item--offline')
+
+                item.innerText = user.username
+
+                userList.appendChild(item)
+            })
+        } catch (error) {
+            console.error(error)
+
+            alert(error.message)
+        }
+    }
+
+    homeButton.onclick = function () {
+        homeButton.style.display = 'none'
+        chatSection.style.display = 'none'
+
+        postListSection.style.display = ''
+        footer.style.display = ''
+        chatButton.style.display = ''
+    }
 })()
