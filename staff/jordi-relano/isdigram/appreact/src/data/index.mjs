a0e11435a1d977ec3b1f0cdccac84302f2236cd5
JordiRZ <jordirz199444@gmail.com>
2024-03-27 00:34:24 +0100

    react class app edit post+ basic chat #327

---
+++ staff/jordi-relano/isdigram/appreact/src/data/index.mjs
@@ -0,0 +1,26 @@
+// te viene de logic.js donde está ubicado localStorage.users = JSON.stringify(users) después de haber pusheado el username y todo.
+
+
+// var users = localStorage.users ? JSON.parse(localStorage.users) : []
+
+//here we have an array with all the information about the users, so 1 object for user
+
+// var users = []
+// if(localStorage.users){
+//   users = JSON.parse(localStorage.users);
+// }else {
+//     return users;
+// }
+
+// helper, aquí creamos las funciones helpers para encapsular, sólo se realizan aquí dentro ( antes de empezar la función grande)
+
+// data layer
+import Collection from './Collection.mjs'
+
+const db = {
+    users: new Collection('users'),
+    posts: new Collection('posts'),
+    chats: new Collection('chats')
+}
+
+export default db
---
+++ staff/jordi-relano/isdigram/appreact/src/logic.spec.mjs
@@ -0,0 +1,46 @@
+import db from './data/index.mjs'
+import logic from './logic.mjs'
+
+describe('logic', () => {
+    describe('registerUser', () => {
+        it('succeeds a new user', () => {
+            data.users.deleteOne( (user) => {
+                return user.username === 'peperoni'
+            })
+
+            logic.registerUser('Pepe Roni', '2000-01-01', 'pepe@roni.com', 'peperoni', '123qwe123')
+
+            const user = data.users.findOne((user) => {
+                return user.username === 'peperoni'
+            })
+
+            expect(!!user).toBe(true)
+            expect(user.name).toBe('Pepe Roni')
+            expect(user.birthdate).toBe('2000-01-01')
+            expect(user.email).toBe('pepe@roni.com')
+            expect(user.username).toBe('peperoni')
+            expect(user.password).toBe('123qwe123')
+        })
+
+        it('fails on existing users', () => {
+            data.users.deleteOne(function (user) {
+                return user.username === 'peperoni'
+            })
+
+            data.users.insertOne({ name: 'Pepe Roni', birthdate: '2000-01-01', email: 'pepe@roni.com', username: 'peperoni', password: '123qwe123' })
+
+            let errorThrown
+
+            try {
+                logic.registerUser('Pepe Roni', '2000-01-01', 'pepe@roni.com', 'peperoni', '123qwe123')
+            } catch (error) {
+                errorThrown = error
+            }
+
+            expect(errorThrown).toBeInstanceOf(Error)
+            expect(errorThrown.message).toBe('user already exists')
+        })
+    })
+
+    // TODO test all methods
+})
